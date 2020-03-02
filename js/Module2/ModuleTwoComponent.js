b140f9a00242ad1aa50f74a9099a7461e67eee81
gopalmamtani <gopal.mamtani@cognizant.com>
2020-03-02 08:54:35 -0600

    made component two function and used redux hooks

--- js/Module2/ModuleTwoComponent.js
+++ js/Module2/ModuleTwoComponent.js
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
+import { useSelector } from 'react-redux';
 
-export default function ModuleTwoComponent() {
-  return (
+export default function ModuleTwoComponent () {
+  
+    const ageCounterVal = useSelector(state => state.age.age);
+    return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+      <Text>Home Screen {ageCounterVal}</Text>
     </View>
   );
 }
